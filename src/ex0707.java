159dd9a20ae49e7d23d57ca311b6f763fc2f9df9
Isak Jung <isack19999@gmail.com>
2023-07-08 00:10:23 +0900

    Add

--- src/ex0707.java
+++ src/ex0707.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class ex0707 {
 
     /**
@@ -23,7 +29,55 @@ public class ex0707 {
      * 첫째 줄에 갑옷을 만들 수 있는 개수를 출력한다.
      */
 
-    public static void main(String[] args) {
+    /**
+     * 두 재료의 번호의 합, 즉 크기를 비교하므로 값을 정렬하면 문제를 좀 더 쉽게 풀 수 있다.
+     * N의 최대 범위가 15,000이므로 O(nlogn) 시간 복잡도 알고리즘을 사용해도 문제가 없다.
+     * 일반적으로 정렬 알고리즘의 시간 복잡도는 O(nlogn)이므로 정렬을 사용해도 괜찮다.
+     * 입력한 N개의 재룟ㄱ밧을 정렬한 다음 양쪽 끝의 위치를 투 포인터로 지정해 문제에 접근해 보자
+     */
+
+    /**
+     * 슈도코드
+     * n(재료의 개수), m(갑옷이 되는 번호) 저장하기
+     * for(n만큼 반복)
+     * {
+     *     재료 배열 저장하기
+     * }
+     * 재료 배열 정렬하기
+     * while(i<j)
+     * {
+     *     if(재료 합 < m) 작은 번호 재료를 한 칸 위로 변경하기
+     *     else if(재료 합 > m) 큰 번호 재료를 한 칸 아래로 변경하기
+     *     else count 증가, 양쪽 index 각각 변경하기
+     * }
+     * count 출력하기
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int count = 0;
+        int i = 0; //A[0] -> Min
+        int j = n - 1; // A[n-1] -> Max
+        while (i < j) {
+            if (A[i] + A[j] < m) {
+                i++;
+            } else if (A[i] + A[j] > m) {
+                j--;
+            } else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(count);
 
     }
 }
