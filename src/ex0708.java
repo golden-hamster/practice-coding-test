950e1bfe9756791cd69baaa234623967af983d47
Isak Jung <isack19999@gmail.com>
2023-07-09 00:26:21 +0900

    Add

--- src/ex0708.java
+++ src/ex0708.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class ex0708 {
 
     /**
@@ -38,6 +43,108 @@ public class ex0708 {
      * 이 때 부분 문자열의 길이가 p이므로 슬라이딩 윈도우의 개념을 이용하면 문제를 쉽게 해결할 수 있다.
      */
 
+    /**
+     * 슈도코드
+     * //데이터 저장
+     * s(문자열 크기) p(부분 문자열의 크기)
+     * a(문자열 데이터)
+     * checkArr(비밀번호 체크 배열)
+     * //변수 선언
+     * myArr(현재 상태 배열)
+     * checkSecret(몇 개의 문자와 관련된 개수를 충족했는지 판단하는 변수)
+     * p의 범위(0 ~ p -1)만큼 s 배열에 적용하고, 유요한 비밀번호인지 판단
+     * for(i를 p에서 s까지 반복)
+     * {
+     *     j 선언(i-p)
+     * }
+     */
 
+    static int myArr[];
+    static int checkArr[];
+    static int checkSecret;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int result = 0;
+        checkArr = new int[4];
+        myArr = new int[4];
+        char a[] = new char[s];
+        checkSecret = 0;
+
+        a = bf.readLine().toCharArray();
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if (checkArr[i] == 0) {
+                checkSecret++; // checkArr[i] == 0 일 때 add 함수에서는 판별이 안되기 때문에
+            }
+        }
+
+        for (int i = 0; i < p; i++) { // 부분문자열 청므 받을 때 세팅
+            add(a[i]);
+        }
+
+        if(checkSecret == 4) result++;
+
+        //슬라이딩윈도우
+        for (int i = p; i < s; i++) { //인덱스 주의 요망
+            int j = i-p;
+            add(a[i]);
+            remove(a[j]);
+            if(checkSecret == 4) result++;
+        }
+
+        System.out.println(result);
+        bf.close();
+    }
+
+    private static void remove(char c) {
+        switch (c) {
+            case 'A':
+                myArr[0]++;
+                if(myArr[0] == checkArr[0]) checkSecret--;
+                myArr[0]--;
+                break;
+            case 'C':
+                myArr[1]++;
+                if(myArr[1] == checkArr[1]) checkSecret--;
+                myArr[1]--;
+                break;
+            case 'G':
+                myArr[2]++;
+                if(myArr[2] == checkArr[2]) checkSecret--;
+                myArr[2]--;
+                break;
+            case 'T':
+                myArr[3]++;
+                if(myArr[3] == checkArr[3]) checkSecret--;
+                myArr[3]--;
+                break;
+        }
+    }
+
+    private static void add(char c) {
+        switch (c) {
+            case 'A':
+                myArr[0]++;
+                if(myArr[0] == checkArr[0]) checkSecret++;
+                break;
+            case 'C':
+                myArr[1]++;
+                if(myArr[1] == checkArr[1]) checkSecret++;
+                break;
+            case 'G':
+                myArr[2]++;
+                if(myArr[2] == checkArr[2]) checkSecret++;
+                break;
+            case 'T':
+                myArr[3]++;
+                if(myArr[3] == checkArr[3]) checkSecret++;
+                break;
+        }
+    }
 
 }
